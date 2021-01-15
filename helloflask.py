931a73730590453d8aca2331e62cbf0abd3b9e27
Harmon <Harmon012@gmail,com>
2021-01-15 07:15:23 +0530

    done till logging and downloading log

--- helloflask.py
+++ helloflask.py
@@ -1,17 +1,16 @@
 from flask import Flask, redirect, url_for, request, render_template, flash
 import myroutes 
 
-
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 ##adding routes
 app.add_url_rule('/', view_func=myroutes.home)
 app.add_url_rule('/register', view_func=myroutes.register)
 app.add_url_rule('/register_user', view_func=myroutes.submit_file, methods=['GET', 'POST'])
 app.add_url_rule('/login', view_func=myroutes.login)
-app.add_url_rule('/loginAuth', view_func=myroutes.loginAuth,methods=['GET', 'POST'])
-app.add_url_rule('/api_call', view_func=myroutes.api_call)
-app.add_url_rule('/download', view_func=myroutes.download)
+app.add_url_rule('/loginAuth', view_func=myroutes.loginAuth,methods=['POST'])
+app.add_url_rule('/profile/<user_name>/<img_file_path>', view_func=myroutes.profile)
+app.add_url_rule('/sendlogdata/<user_name>/<type>', view_func = myroutes.sendlogdata)
 
 #main
 if __name__ == '__main__':
--- myroutes.py
+++ myroutes.py
@@ -1,16 +1,14 @@
-from flask import  request,  send_file, render_template, redirect, jsonify
-from openpyxl import load_workbook
+from flask import  request,  send_file, render_template, redirect, jsonify, url_for
+from openpyxl import load_workbook, Workbook
   
-wb = load_workbook('RegisteredUsers.xlsx') 
-sheet1 = wb.active 
-
-print("done setting")
 
 def submit_file():
+    wb = load_workbook('RegisteredUsers.xlsx') 
+    sheet1 = wb.active  
     top=sheet1.max_row+1
     f = request.files['pp']
     if(f.filename!=''):
-        f.save('files/'+f.filename)
+        f.save('static/files/'+f.filename)
 
         sheet1["A"+str(top)] = request.form['name']
         sheet1["B"+str(top)] = request.form['email']  
@@ -33,20 +31,58 @@ def register():
 def login():
     return render_template('login.html')
 
-def api_call():
-    return render_template('download_file.html', filename="college_id.jpg")
 
-def download():
-    return send_file("files/college_id.jpg")
+
+def sendlogdata(user_name, type):
+    if(type == "xlsx"):
+        wb2 = load_workbook('Logindata.xlsx')
+        loginsheet = wb2.active 
+        tempwb = Workbook()
+        tempsheet = tempwb.active
+        tempsheettop = 1
+        tempsheet["A"+str(tempsheettop)] = "Date"
+        tempsheet["B"+str(tempsheettop)] = "Time"
+
+        for i in range(2, loginsheet.max_row):
+            if(loginsheet["A"+str(i)].value == user_name ):
+                tempsheet["A"+str(tempsheettop+1)] = loginsheet["B"+str(i)].value
+                tempsheet["B"+str(tempsheettop+1)] = loginsheet["C"+str(i)].value
+                tempsheettop = tempsheettop+1
+
+        tempwb.save(user_name+".xlsx")
+        return send_file(user_name+".xlsx", cache_timeout=0)
+        
+    
+def logLogin(user_name):
+    wb2 = load_workbook('Logindata.xlsx')
+    loginsheet = wb2.active
+    top = loginsheet.max_row+1
+    loginsheet["A"+str(top)] = user_name
+    from datetime import datetime
+    time = datetime.now().strftime("%H:%M:%S")
+    date = datetime.now().strftime("%d/%m/%y")
+    loginsheet["B"+str(top)] = date
+    loginsheet["C"+str(top)] = time
+    wb2.save('LoginData.xlsx')
+
+def profile(user_name, img_file_path):
+    #saving time and date data to file
+    logLogin(user_name)
+    img_path ="\\static\\files\\"+img_file_path
+    return render_template('profile_page.html', name=user_name, src=img_path)
+
 
 def loginAuth():
+    wb = load_workbook('RegisteredUsers.xlsx') 
+    sheet1 = wb.active 
     email = request.form["email"]
     password = request.form["password"] 
-    print(sheet1.active_cell)
     for row in range(2, sheet1.max_row+1):
         if(sheet1['B'+str(row)].value==email):
             if(sheet1['C' + str(row)].value == password):
-                return 'login successfull!"> Hello %s ' %(sheet1['A' + str(row)].value)
+                return redirect(url_for('profile', user_name =sheet1['A' + str(row)].value, img_file_path =sheet1['D' + str(row)].value) )
             
     return 'login failed'
 
+# $env:FLASK_APP = "helloflask.py"
+# $env:FLASK_ENV = "development"
